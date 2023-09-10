Fixed some multihead attention problems and changed masking. Results are better but still end up only preidcintg 0s and filling the entire block

scripts/preprocessing.py
@@ -59,6 +59,8 @@ def tokenize(data, en_tokenizer, es_tokenizer, block_size):
     eos = en_tokenizer.eos_id()
     
     for row in data:
+        # print(row[0])
+        # print(row[1])
         en = torch.tensor(en_tokenizer.EncodeAsIds(row[0]), dtype=torch.long)
         es = torch.tensor(es_tokenizer.EncodeAsIds(row[1]), dtype=torch.long)
 
@@ -83,13 +85,36 @@ def tokenize(data, en_tokenizer, es_tokenizer, block_size):
 
     return tokenized_data
 
-# en_sp = spm.SentencePieceProcessor()
-# en_sp.Load("models/sentencepiece_model_10k_english.model")
+en_sp = spm.SentencePieceProcessor()
+en_sp.Load("models/sentencepiece_model_10k_english.model")
 
-# es_sp = spm.SentencePieceProcessor()
-# es_sp.Load("models/sentencepiece_model_16k_spanish.model")
+es_sp = spm.SentencePieceProcessor()
+es_sp.Load("models/sentencepiece_model_10k_spanish.model")
 
+# test = tokenize(split_data(), en_sp, es_sp, 96) # 0 is english
 
+# enc = es_sp.EncodeAsIds("¿sabés por qué pasa?")
+# enc.insert(0, 1)
+# enc.append(2)
+
+# # Pad the list to size 96
+# desired_size = 96
+# if len(enc) < desired_size:
+#     padding = [0] * (desired_size - len(enc))
+#     enc = enc + padding
+
+# print(enc)
+# print(test[0][1].tolist())
+
+# for pair in test:
+#     if pair[1].tolist() == enc:
+#         print(pair[0])
+
+print(en_sp.DecodeIds([5-1,  25-1, 107-1, 277-1, 393-1,  11-1, 276-1,   4-1]))
+print(es_sp.DecodeIds([95-1,  70-1, 257-1,  65-1, 193-1,   4-1]))
+
+# print(es_sp.EncodeAsIds("¿Hola Cómo te llamas?"))
+# print(es_sp.DecodeIds([1, 13, 3, 69, 300, 25, 3, 41, 1279, 35, 1964, 12]))
 # print(tokenize(split_data(), en_sp, es_sp, 256)[0]) # 0 is english
-# print(tokenize(split_data(), en_sp, es_sp, 256)[1]) # 1 is spanish
-# print(torch.tensor(es_sp.EncodeAsIds("como suele haber varias páginas web sobre cualquier tema, normalmente sólo le doy al botón de retroceso cuando entro en una página web que tiene anuncios en ventanas emergentes. simplemente voy a la siguiente página encontrada por google y espero encontrar algo menos irritante."), dtype=torch.long).size(0))
+# print(tokenize(split_data(), en_sp, es_sp, 256)[500]) # 1 is spanish
+# print(torch.tensor(es_sp.EncodeAsIds("como suele haber varias páginas web sobre cualquier tema, normalmente sólo le doy al botón de retroceso cuando entro en una página web que tiene anuncios en ventanas emergentes. simplemente voy a la siguiente página encontrada por google y espero encontrar algo menos irritante."), dtype=torch.long))

scripts/train.py
@@ -6,20 +6,21 @@ import random
 import time
 import gc
 
-batch_size = 24 # number of sequences per batch
+batch_size = 32 # number of sequences per batch
 block_size = 96 # max length of sequence/characters per sequence
-# max_iters = 2000 # number of iterations to train for
-# eval_iters = 200 # iterations to evaluate model performance
-# eval_interval = 250 # interval to evaluate model performance
-max_iters = 20 # number of iterations to train for
-eval_iters = 5 # iterations to evaluate model performance
-eval_interval = 4 # interval to evaluate model performance
-learning_rate = 3e-4
+max_iters = 2000 # number of iterations to train for
+eval_iters = 200 # iterations to evaluate model performance
+eval_interval = 250 # interval to evaluate model performance
+# max_iters = 20 # number of iterations to train for
+# eval_iters = 5 # iterations to evaluate model performance
+# eval_interval = 4 # interval to evaluate model performance
+learning_rate = 1e-6
 n_embed = 384 # embedding dimension
-n_head = 6 # number of heads
+n_head = 8 # number of heads
 head_size = n_embed // n_head # size of each head
 n_layer = 6 # number of layers
 dropout = 0.2
+translate_interval = 100
 
 start_time = time.time()
 gc.collect()
@@ -39,10 +40,10 @@ print(f"Name of current CUDA device:{torch.cuda.get_device_name(cuda_id)}")
     Split and Tokenize Data
 """
 en_sp = spm.SentencePieceProcessor()
-en_sp.Load("models/sentencepiece_model_10k_english.model")
+en_sp.Load("models/sentencepiece_model_10k_english2.model")
 
 es_sp = spm.SentencePieceProcessor()
-es_sp.Load("models/sentencepiece_model_16k_spanish.model")
+es_sp.Load("models/sentencepiece_model_10k_spanish.model")
 
 data = preprocessing.tokenize(preprocessing.split_data(), en_sp, es_sp, block_size)
 
@@ -62,6 +63,29 @@ train_data = data[:train_size]
 val_data = data[train_size:]
 
 
+# enc = es_sp.EncodeAsIds("si quieres sonar como un hablante nativo, debes estar dispuesto a practicar diciendo la misma frase una y otra vez de la misma manera en que un músico de banjo practica el mismo fraseo una y otra vez hasta que lo puedan tocar correctamente y en el tiempo esperado.")
+# enc.insert(0, 1)
+# enc.append(2)
+
+# # Pad the list to size 96
+# desired_size = 96
+# if len(enc) < desired_size:
+#     padding = [0] * (desired_size - len(enc))
+#     enc = enc + padding
+
+# print(enc)
+# # print(train_data[0][1].tolist())
+
+# for pair in train_data:
+#     if pair[1].tolist() == enc:
+#         print(pair[0])
+#         print(en_sp.DecodeIds(pair[0].tolist()))
+
+# for pair in val_data:
+#     if pair[1].tolist() == enc:
+#         print(pair[0])
+#         print(en_sp.DecodeIds(pair[0].tolist()))
+
 """
     get_batch
     using the array of pairs randomly select batch size of pairs and return them
@@ -71,27 +95,46 @@ def get_batch(split):
 
     ix = torch.randint(len(data) - block_size, (batch_size,)) # generates n numbrs based on btach_size that represent an index from data, randomally choosing four places to parallel train
 
+    # enc = es_sp.EncodeAsIds("no vuelvas nunca.")
+    # enc.insert(0, 1)
+    # enc.append(2)
+
+    # # Pad the list to size 96
+    # desired_size = 96
+    # if len(enc) < desired_size:
+    #     padding = [0] * (desired_size - len(enc))
+    #     enc = enc + padding
+
+    # print(enc)
+
+    # for pair in data:
+    #     if pair[1].tolist() == enc:
+    #         print(pair[0])
+    #         print(en_sp.DecodeIds(pair[0].tolist()))
+
     max_len = 0
 
     x_tensors = []
     y_tensors = []
 
+    # print(ix)
     for i in ix:
-      t = data[i:i+block_size][0][0]
-      max_len = max(max_len, t.size(0))
+      # print("Row: ", data[i])
+      t = data[i][0]
+      # print("X:", en_sp.DecodeIds(t.tolist()))
       x_tensors.append(t)    
-      t = data[i:i+block_size][0][1]
+      t = data[i][1]
+      # print("Y:", es_sp.DecodeIds(t.tolist()))
       y_tensors.append(t)
 
-    # for i in range(len(x_tensors)):
-    #   pad_size = max_len - x_tensors[i].shape[0]
-
-    #   # Create pad tensor  
-    #   pad = torch.zeros(pad_size, dtype=torch.long)
-
-    #   # Concatenate padding
-    #   x_tensors[i] = torch.cat([x_tensors[i], pad])
-    #   y_tensors[i] = torch.cat([y_tensors[i], pad])
+    # for i in ix:
+    #   print("Row: ", data[i])
+    #   t = data[i:i+block_size][0][0]
+    #   print("X:", en_sp.DecodeIds(t.tolist()))
+    #   x_tensors.append(t)    
+    #   t = data[i:i+block_size][0][1]
+    #   print("Y:", es_sp.DecodeIds(t.tolist()))
+    #   y_tensors.append(t) 
 
     x = torch.stack(x_tensors)      
     y = torch.stack(y_tensors)  
@@ -113,11 +156,34 @@ def estimate_loss():
     losses = torch.zeros(eval_iters)
     for k in range(eval_iters):
        X, Y = get_batch(split)
-       logits, loss = model(X, Y)
+       src_mask, target_mask = create_mask(X, Y)
+       logits, loss = model(X, Y, src_mask, target_mask)
        losses[k] = loss.item()
     out[split] = losses.mean()
   model.train()
   return out
+
+"""
+  Create mask 
+  
+  creates the src and target masks
+  mask out any padding tokens 
+  
+  Input
+    the x and y batch
+
+  Output 
+    the src and target masks (B,L,L)  L = seq_len or block_size
+"""
+def create_mask(src, target, seq_len=block_size):
+  e_mask = (src != 0).unsqueeze(1)  # (B, 1, L)
+  d_mask = (target != 0).unsqueeze(1)  # (B, 1, L)
+
+  nopeak_mask = torch.ones([1, seq_len, seq_len], dtype=torch.bool)  # (1, L, L)
+  nopeak_mask = torch.tril(nopeak_mask).to(device)  # (1, L, L) to triangular shape
+  d_mask = (d_mask & nopeak_mask)  # (B, L, L) padding false
+
+  return e_mask.to(device), d_mask.to(device)
 
 
 """
@@ -145,6 +211,7 @@ def train_model(m):
       print(f"Current memory usage: {current_memory / (1024 ** 2):.2f} MB")
       print(f"Peak memory usage: {peak_memory / (1024 ** 2):.2f} MB")
 
+    if iter % translate_interval == 0:
       test_src = torch.tensor(en_sp.EncodeAsIds("Hello what is your name?"), dtype=torch.long, device=device)
       test_src = torch.cat([torch.tensor([1], dtype=torch.long, device=device), test_src, torch.tensor([2], dtype=torch.long, device=device)]) 
 
@@ -155,15 +222,17 @@ def train_model(m):
 
       print("Hello what is your name? : ", es_sp.DecodeIds(m.generate(test_src, block_size).tolist()))
 
-    # sample a batch of data
-    xb, yb = get_batch('train')
-
     # clears memory
     gc.collect() 
     torch.cuda.empty_cache()
 
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    src_mask, trg_mask = create_mask(xb, yb)
+
     # evaluate the loss
-    logits, loss = m.forward(xb, yb)
+    logits, loss = m.forward(xb, yb, src_mask, trg_mask)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
@@ -190,6 +259,13 @@ vocab_size_y = len(es_sp)
 
 model = Transformer(n_embed, n_head, dropout, block_size, vocab_size_x, vocab_size_y, n_layer).to(device)
 train_model(model)
+
+# print(get_batch('train'))
+# x, y = get_batch('train')
+# print(en_sp.DecodeIds(x[0].tolist()[1:-1]))
+# print(es_sp.DecodeIds(y[0].tolist()[1:-1]))
+# print(len(x))
+# print(len(y))
 
 # tok_data = preprocessing.tokenize(preprocessing.split_data())
 # batched_data = get_batch(tok_data)

scripts/transformer.py
@@ -4,6 +4,7 @@ from torch import Tensor
 import numpy as np
 import math
 from torch.nn import functional as F
+import matplotlib.pyplot as plt
 
 
 batch_size = 64 # number of sequences per batch
@@ -32,6 +33,7 @@ class PositionalEncoding(nn.Module):
     def __init__(self, d_model, dropout, max_len=5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
+        self.d_model = d_model
 
         # d_model is the dimension of the embedding vector
         position = torch.arange(0, max_len, device=device).unsqueeze(1)
@@ -41,6 +43,7 @@ class PositionalEncoding(nn.Module):
         self.pe[:, 0, 1::2] = torch.cos(position * div_term)
 
     def forward(self, x):
+        x = x * math.sqrt(self.d_model)
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
@@ -61,23 +64,49 @@ class MultiHeadAttention(nn.Module):
         self.num_heads = num_heads
         self.n_embed = n_embed
 
+        self.counter = 0
+
+    def visualize_attention(self, wei):
+        # Plot attention scores
+        layer_num = 1  # Specify the layer number you want to visualize
+        head_num = 0   # Specify the attention head you want to visualize
+
+        # Get the attention scores for the specified layer and head
+        attention_map = wei[0][0].cpu().detach().numpy()
+        # print(attention_map.shape)
+
+        # Plot the attention heatmap
+        plt.figure(figsize=(10, 8))
+        plt.imshow(attention_map, cmap='viridis', aspect='auto')
+        plt.title(f"Layer {layer_num}, Head {head_num} Attention")
+        plt.xlabel("Input Tokens")
+        plt.ylabel("Output Tokens")
+        plt.colorbar()
+        plt.show()
+
+
     def forward(self, query, key, value, mask=None):
         B, T, C = query.shape  # B is batch size, T is sequence length, C is n_embed
         # d_k is n_embed
 
         # Perform linear transformation
         q, k, v = self.query(query), self.key(key), self.value(value)
-        # q = self.query(query)
-        k = k.view(B, T, self.n_embed, C // self.n_embed).transpose(1, 2)  # (B, num_heads, T, head_size)
-        q = q.view(B, T, self.n_embed, C // self.n_embed).transpose(1, 2)  # (B, num_heads, T, head_size)
-        v = v.view(B, T, self.n_embed, C // self.n_embed).transpose(1, 2)  # (B, num_heads, T, head_size)
-
+        # k = k.view(B, T, self.n_embed, C // self.n_embed).transpose(1, 2)  # (B, num_heads, T, head_size)
+        # q = q.view(B, T, self.n_embed, C // self.n_embed).transpose(1, 2)  # (B, num_heads, T, head_size)
+        # v = v.view(B, T, self.n_embed, C // self.n_embed).transpose(1, 2)  # (B, num_heads, T, head_size)
+        k = k.view(B, -1, self.num_heads, self.n_embed // self.num_heads).transpose(1, 2)  # (B, num_heads, T, head_size)
+        q = q.view(B, -1, self.num_heads, self.n_embed // self.num_heads).transpose(1, 2)  # (B, num_heads, T, head_size)
+        v = v.view(B, -1, self.num_heads, self.n_embed // self.num_heads).transpose(1, 2)  # (B, num_heads, T, head_size)
         with torch.cuda.amp.autocast_mode.autocast(enabled=False):
 
             wei = q @ k.transpose(-2, -1) * C **-0.5  # C is head size
 
             if mask!=None:
-                wei = wei.masked_fill(mask[:T, :T]==0, -1 * 1e9) # mask out the upper triangle (B, T, T)
+                mask = mask.unsqueeze(1)
+                wei = wei.masked_fill(mask==0, -1 * 1e9) # mask out the upper triangle (B, T, T)
+                # print(wei)
+            # if self.counter % 250 == 0:
+            #     self.visualize_attention(wei)
 
             wei = F.softmax(wei, dim=-1)  # normalize
 
@@ -88,7 +117,7 @@ class MultiHeadAttention(nn.Module):
             # if mask==None: print("Encoder: ")
             # else: print("Decoder: ")
             # print(out)
-
+        self.counter+=1
         out = self.dropout(self.proj(out))
         return out
 
@@ -132,13 +161,13 @@ class Decoder(nn.Module):
         self.ffwd = FeedForward(n_embed, dropout)
         self.ln3 = nn.LayerNorm(n_embed)
 
-    def forward(self, x, enc_output):
+    def forward(self, x, enc_output, enc_mask, dec_mask):
         B, T, C= x.shape
 
         x_1 = self.ln1(x)
-        x = x + self.sa_heads(x_1, x_1, x_1, self.tril[:T, :T] == 0)
+        x = x + self.sa_heads(x_1, x_1, x_1, dec_mask)
         x_2 = self.ln2(x)
-        x = x + self.cross_attention(x_2, enc_output, enc_output, torch.ones((block_size, block_size), device=device))
+        x = x + self.cross_attention(x_2, enc_output, enc_output, enc_mask)
         x = x + self.ffwd(self.ln3(x))
         
         return x
@@ -162,11 +191,11 @@ class Encoder(nn.Module):
         self.ffwd = FeedForward(n_embed, dropout)
         self.ln2 = nn.LayerNorm(n_embed)
 
-    def forward(self, x):
+    def forward(self, x, enc_mask):
         B, T, C= x.shape
 
         x_1 = self.ln1(x)
-        x = x + self.sa_heads(x_1, x_1, x_1, torch.ones((block_size, block_size), device=device))
+        x = x + self.sa_heads(x_1, x_1, x_1, enc_mask)
         x = x + self.ffwd(self.ln2(x))
         return x
 
@@ -191,22 +220,27 @@ class Transformer(nn.Module):
         self.ln_f = nn.LayerNorm(n_embed)    # final layer norm
         self.lm_head = nn.Linear(n_embed, vocab_size_y) # paramters are in_features, out_features
 
-    def forward(self, x, targets):
+    def forward(self, x, targets, src_mask, target_mask):
 
         x_tok_emb = self.token_embedding_table_x(x)
         y_tok_emb = self.token_embedding_table_y(targets)
 
         x_pos_enc = self.pos_enc(x_tok_emb)
         y_pos_enc = self.pos_enc(y_tok_emb)
+        
+        # print("target shape: ", y_pos_enc.shape)
 
         enc_output = x_pos_enc
         for encoder in self.encoder_layers:
-          enc_output = encoder(enc_output)
+          enc_output = encoder(enc_output, src_mask)
 
         dec_output = y_pos_enc
         for decoder in self.decoder_layers:
-          dec_output = decoder(dec_output, enc_output) 
+          dec_output = decoder(dec_output, enc_output, src_mask, target_mask) 
         
+        # print("before final linear forward shape: ", dec_output.shape)
+
+
         res = self.ln_f(dec_output)
         logits = self.lm_head(res)
 
@@ -215,12 +249,17 @@ class Transformer(nn.Module):
         else:
             # pytorch wants a (B, C, T) tensor for cross_entropy so we need some reshaping
             B, T, C = logits.shape
-            # print(logits.shape)
-            # print(targets.shape)
+            # print("Logits: ", logits.shape)
+            # print("Targets: ", targets.shape)
+            # print(targets)
             logits = logits.view(B*T, C)
             targets = targets.view(B*T)
+            # torch.set_printoptions(threshold=10_000)
+            # print("Logits: ", logits)
+            # print("Targets: ", targets)
             loss = F.cross_entropy(logits, targets)
 
+        # print(logits.shape)
         return logits, loss
 
 
@@ -237,90 +276,108 @@ class Transformer(nn.Module):
     """
     # the job of generate is to extend idx to be B by T+1, B by T+2 ....
     def generate(self, src, seq_len):
-        # start = torch.tensor([1], dtype=torch.long, device=device)
-        # length = max_new_tokens - 1
-        # zeros = torch.zeros(length, dtype=torch.long, device=device)
-        # target_input = torch.cat([start, zeros])
-
         # self.dropout = 0 # turn off dropout
 
         # self.eval() # put model in eval mode
 
         # src = torch.stack([src])
-        # target_input = torch.stack([target_input])
+        # src = self.token_embedding_table_x(src)
+        # src = self.pos_enc(src)
+        # enc_output = src
+        # for encoder in self.encoder_layers:
+        #   enc_output = encoder(enc_output)
 
-        # logits, loss, = self(src, target_input) # call forward
+        # # print(enc_output.shape)
+
+        # target = torch.zeros(seq_len).long().to(device)
+        # target[0] = 1   # set the first token to be the start token
+        # target = torch.stack([target])
+        # target_len = 0
+
+        # for i in range(1, seq_len):
+        #     # print(target)
+        #     target_embed = self.token_embedding_table_y(target)
+        #     target_enc = self.pos_enc(target_embed)
+
+        #     # print("target shape: ", target_enc.shape)
+
+        #     dec_output = target_enc
+        #     for decoder in self.decoder_layers:
+        #         dec_output = decoder(dec_output, enc_output)
         
-        # res = torch.tensor([0], dtype=torch.long, device=device).unsqueeze(0)
-        # # idx is (B, T) array of indicies in the current context
-        # for i in range(max_new_tokens):
-        #     # gets the predictions
-        #     # print(src.shape)
-        #     # print(target_input.shape)
-        #     # print(logits.shape)
-        #     # focus only on the last time step
-        #     # logits = logits[: , -1, :]  # becomes (B, C)
-        #     # apply softmax to normalize and get probabilities
-        #     probs = F.softmax(logits[i], dim=-1) # dim are (B, C)
-        #     # sample from distribution to get a single prediction for what char comes next
-        #     src_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+        #     dec_output = self.ln_f(dec_output)
+        #     dec_output = self.lm_head(dec_output)
+        #     res = F.softmax(dec_output, dim=-1)
+
+        #     # res = F.softmax(self.lm_head(self.ln_f(dec_output)), dim=-1)
+
+        #     # added this to make the result match logits (batch_size * seq_len, vocab_size)
+        #     B, T, C = res.shape
+        #     res = res.view(B*T, C)
+
+        #     # print("dec shape:", res.shape)
+        #     res = torch.argmax(res, dim=-1) # (1, seq_len)
+
+        #     # print(res)
+
+        #     last_word_id = res[i-1]
+        #     # print(last_word_id)
             
-        #     # append sampled index to the running sequence
-        #     res = torch.cat((res, src_next.unsqueeze(0)), dim=1) # (B, T+1)
+        #     if last_word_id == 2:
+        #         break
+            
+        #     # print(last_word_id)
+        #     target[0][i] = last_word_id
+        #     target_len = i
+
+        # print(target[0])
         # self.dropout = dropout # turn dropout back on
-        # print(res[0])
-        # return res[0]
+        # self.train()
+        # return target
 
         self.dropout = 0 # turn off dropout
 
         self.eval() # put model in eval mode
 
         src = torch.stack([src])
-        src = self.token_embedding_table_x(src)
-        src = self.pos_enc(src)
-        enc_output = src
-        for encoder in self.encoder_layers:
-          enc_output = encoder(enc_output)
+        src_mask = (src != 0).unsqueeze(1).to(device=device)  # (B, 1, L)
 
-        print(enc_output.shape)
 
         target = torch.zeros(seq_len).long().to(device)
         target[0] = 1   # set the first token to be the start token
         target = torch.stack([target])
         target_len = 0
 
+
         for i in range(1, seq_len):
-            # print(target)
-            target_embed = self.token_embedding_table_y(target)
-            target_enc = self.pos_enc(target_embed)
+            trg_mask = (target != 0).unsqueeze(1)  # (B, 1, L)
+            nopeak_mask = torch.ones([1, seq_len, seq_len], dtype=torch.bool)  # (1, L, L)
+            nopeak_mask = torch.tril(nopeak_mask).to(device)  # (1, L, L) to triangular shape
+            trg_mask = (trg_mask & nopeak_mask) # (B, L, L) padding false
 
-            # print(target_enc.shape)
+            logits, loss = self(src, target, src_mask, trg_mask)
+            # print("loss: ", loss)
 
-            dec_output = target_enc
-            for decoder in self.decoder_layers:
-                dec_output = decoder(dec_output, enc_output)
+            # print("dec shape:", res.shape)
+            res = torch.argmax(logits, dim=-1) # (1, seq_len)
 
-            # print(dec_output)
-            res = F.softmax(self.lm_head(self.ln_f(dec_output)), dim=-1)
-            res = torch.argmax(res, dim=-1) # (1, L)
-
-            # print(res[0][i-1])
             # print(res)
 
-            last_word_id = res[0][i-1].item()
+            last_word_id = res[i-1]
             # print(last_word_id)
+            # print(res[i])
             
-            if last_word_id == 2:
-                break
-            
-            print(last_word_id)
+            # print(last_word_id)
             target[0][i] = last_word_id
             target_len = i
 
+            if last_word_id == 2:
+                break
+
         print(target[0])
         self.dropout = dropout # turn dropout back on
+        self.train()
         return target
-
 
 # import sentencepiece as spm
 

scripts/translate.py
@@ -5,9 +5,9 @@ from transformer import Transformer
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.cuda.set_device(0)
 
-batch_size = 64 # number of sequences per batch
-block_size = 256 # max length of sequence/characters per sequence
-max_iters = 5000 # number of iterations to train for
+batch_size = 24 # number of sequences per batch
+block_size = 96 # max length of sequence/characters per sequence
+max_iters = 2000 # number of iterations to train for
 eval_iters = 200 # iterations to evaluate model performance
 eval_interval = 250 # interval to evaluate model performance
 learning_rate = 3e-4
@@ -17,34 +17,38 @@ head_size = n_embed // n_head # size of each head
 n_layer = 6 # number of layers
 dropout = 0.2
 
-sp = spm.SentencePieceProcessor()
-sp.Load("models/sentencepiece_model_16k.model")
-vocab_size_x = len(sp)
-vocab_size_y = len(sp)
+en_sp = spm.SentencePieceProcessor()
+en_sp.Load("models/sentencepiece_model_10k_english.model")
+
+es_sp = spm.SentencePieceProcessor()
+es_sp.Load("models/sentencepiece_model_16k_spanish.model")
+
+vocab_size_x = len(en_sp)
+vocab_size_y = len(es_sp)
 
 model = Transformer(n_embed, n_head, dropout, block_size, vocab_size_x, vocab_size_y, n_layer).to(device)
-checkpoint = torch.load("models/test_model.pth")
+checkpoint = torch.load("models/model_two_tok.pth")
 model.load_state_dict(checkpoint)
 
-en = torch.tensor(sp.EncodeAsIds("How are you?"), dtype=torch.long, device=device)
-es = torch.tensor(sp.EncodeAsIds("Cómo estás"), dtype=torch.long, device=device)
+en = torch.tensor(en_sp.EncodeAsIds("How are you?"), dtype=torch.long, device=device)
+es = torch.tensor(es_sp.EncodeAsIds("Cómo estás"), dtype=torch.long, device=device)
 
 print(en)
 print(es)
 
-bos = sp.bos_id()  
-eos = sp.eos_id()
+bos = es_sp.bos_id()  
+eos = es_sp.eos_id()
 en = torch.cat([torch.tensor([bos], device=device), en, torch.tensor([eos], device=device)]) 
 
 len1 = en.size(0)
-len2 = 6
+len2 = 96   # block size
 
 # Pad tensors if needed
 if len1 < len2:
     padding = torch.zeros(len2 - len1, device=device , dtype=torch.long)
     en = torch.cat([en, padding])
 
-# print(en)
+print(en)
 
-print(sp.DecodeIds(model.generate(en, len2).tolist()))
+print(en_sp.DecodeIds(model.generate(en, len2).tolist()))
 